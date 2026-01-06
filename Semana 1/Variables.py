9bd1abb77db73fea324a76c4488cc09fbfe9f7a5
Dy0123Flores <floresreyesdylanandres@gmail.com>
2026-01-06 11:40:31 -0600

    Update Variables.py

---
+++ Semana 1/Variables.py
@@ -0,0 +1,31 @@
+# #Variables
+# nombre = "Dylan"
+# edad= 21
+# Cumple= 3.5
+# Estado= True
+
+# Edades= [12,56,54,45]
+
+# print(nombre, edad)
+
+# for items in Edades:
+#     print(items)
+
+# for i in range(10):
+#     print("I: ",i)
+
+# contador=1
+
+# while contador <20:
+#     print("valor",contador)
+#     contador +=1
+
+password= ""
+
+while password !="2":
+    print("ingrese en el while")
+    password=input("ingrese su password")
+  
+
+if password =="123":
+    print("sistema de inicio")
